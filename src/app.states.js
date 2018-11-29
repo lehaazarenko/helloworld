07298749491f8bed2de4b9fa93d07f8b0bfcdf7f
Aliaksei Azaranka <aliaksei_azaranka@epam.com>
2018-11-29 16:33:27 +0300

    structure-fix: removed most of comments

--- src/app.states.js
+++ src/app.states.js
@@ -1,15 +1,16 @@
 angular.module('helloworld').config(function($stateProvider, $urlRouterProvider) {
 
-  $stateProvider.state('search', {
-    url: '/search',
-    template: '<search></search>'
-  })
-  .state('search.users', {
-    url: '/{dataType}/{data}',
-    params: {
-      dataType: 'users',
-      data: ''
-    },
-    template: '<users></users>'
-  });
+  $stateProvider
+    .state('search', {
+      url: '/search',
+      template: '<search></search>'
+    })
+    .state('search.users', {
+      url: '/{dataType}/{data}',
+      params: {
+        dataType: 'users',
+        data: ''
+      },
+      template: '<users></users>'
+    });
 });
--- src/components/search.controller.js
+++ src/components/search.controller.js
@@ -4,21 +4,7 @@ angular.module('helloworld').controller('searchController', ['$state',  function
 
   ctrl.findData = (dataType) => (data) => {
   	console.log('dataType: ', dataType);
-    // $state.go('search.users', { dataType, data });
     $state.go('search.users', { dataType, data });
-    // $http({
-    //   method: 'GET',
-    //   url: `https://api.github.com/search/${dataType}?q=${data}&per_page=10`
-    // }).then((response) => {
-    //     ctrl.recievedData[dataType] = response.data.items;
-    //     console.log(ctrl.recievedData);
-    //   }, (error) => {
-    //     console.log(error);
-    //   });
-    // };
-
-    // ctrl.findData($stateParams.dataType)($stateParams.data);
-  
   }
 
 }]);
