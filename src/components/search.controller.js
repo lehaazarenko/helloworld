a2555b593e19777dcf513799b36b3b2089ecac99
Aliaksei Azaranka <aliaksei_azaranka@epam.com>
2018-12-04 18:09:32 +0300

    structure-fix: removed stuff intended to be used in http pagination

--- src/components/search.controller.js
+++ src/components/search.controller.js
@@ -7,6 +7,6 @@ angular.module('helloworld').controller('searchController', ['$state',  function
   ctrl.findData = (dataType) => (data) => {
     console.log('dataType: ', dataType);
   	console.log('data: ', data);
-    $state.go('search.users', { dataType: dataType, data: data, pageNumber: '1' });
+    $state.go('search.users', { dataType: dataType, data: data });
   };
 }]);
