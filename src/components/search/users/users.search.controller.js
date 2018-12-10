4eadbb36d300d04741a7bc43c6c4cb4d7e93d865
Aliaksei Azaranka <aliaksei_azaranka@epam.com>
2018-12-10 15:45:22 +0300

    user-details-pagination: done with no console.logs

--- src/components/search/users/users.search.controller.js
+++ src/components/search/users/users.search.controller.js
@@ -1,27 +1,20 @@
 (function(){
   'use strict';
 
-  // angular.module('helloworld').controller('usersController', ['$http', '$scope', '$state', '$stateParams', 'searchFactory',  function($http, $scope, $state, $stateParams, searchFactory) {
   angular.module('helloworld').controller('usersSearchController', ['$state', '$stateParams', '$q', 'searchFactory',  function($state, $stateParams, $q, searchFactory) {
 
     const ctrl = this;
 
-    // console.log('factory: ',searchFactory);
-    // console.log('in users controller');
 
     ctrl.findData = searchFactory.findData;
     ctrl.showUserDetails = searchFactory.showUserDetails;
-    // console.log('showUserDetails: ', ctrl.showUserDetails);
     ctrl.update = searchFactory.update;
     ctrl.initPages = searchFactory.initPages;
     ctrl.isNumberOfPagesValid = searchFactory.isNumberOfPagesValid;
 
-    // console.log('ctrl: ', ctrl);
-
     function init() {
         if (!ctrl.isNumberOfPagesValid($stateParams.pageNumber)) {
           $state.go('search.page-not-found');
-          // console.log('isNumberOfPagesValid: ', isNumberOfPagesValid);
         } else {
           ctrl.recievedData = {
             users: [],
@@ -33,10 +26,6 @@
           ctrl.params = $stateParams;
           ctrl.params.pageNumber = parseInt(ctrl.params.pageNumber);
           ctrl.findData();
-
-          // console.log('find data: ', ctrl);
-
-          // console.log('pageNumber in usersSearch: ', ctrl.params.pageNumber);
 
           ctrl.recievedData = searchFactory.recievedData;
           ctrl.pagesData = searchFactory.pagesData;    
--- src/components/user-details/user-details.controller.js
+++ src/components/user-details/user-details.controller.js
@@ -6,33 +6,19 @@
     const ctrl = this;
     ctrl.isNext = searchFactory.isNext;
     ctrl.isNext($stateParams.index).then((response) => {
-        // console.log('isNext response: ', response);
         ctrl.isNext = response;
     }, (error) => {
         // console.log('isNext error: ', error);
     });
 
-    // console.log(searchFactory.isPrev);
-
     ctrl.isPrev = $stateParams.index > 0;
-    // console.log('ctrl.isPrev', ctrl.isPrev);
 
 
     ctrl.userData = searchFactory.userData;
-    // console.log('State Details: ');
-    // console.log($stateParams);
     ctrl.findUser = searchFactory.findUser;
-    // console.log(ctrl.findUser);
-    // console.log('userData Details: ', ctrl.userData);
-    // console.log('$stateParams in Details: ', $stateParams);
 
     ctrl.findUser($stateParams.index).then((response) => {
-    // ctrl.findUser($stateParams.index, 10).then((response) => {
-        // console.log('response in user-details: ', response);
-        // console.log('isNext value: ', ctrl.isNext);
-        // ctrl.userData = response.data.items[0];
         ctrl.userData = response;
-        // console.log('ctrl.userData', ctrl.userData);
     }, (error) => {
         console.log('kek');
     });
